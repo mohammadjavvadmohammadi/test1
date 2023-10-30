4a98df846902a5bb01215b56d02a161111dc3b5f
mohammadjavvadmohammadi <149230041+mohammadjavvadmohammadi@users.noreply.github.com>
2023-10-29 22:37:43 -0700

    Add files via upload

---
+++ inputt.py
@@ -0,0 +1,15 @@
+#the new project
+kms = int(input("how many kms?"))
+metr = kms*1000
+print(f"you have {kms}kms and convert to {metr}metr")
+
+kg = int(input("how many kg are you?"))
+gr = kg*1000
+print(f"you have {kg}kg and convert to {gr}gr")
+
+m = int(input("how many metr?"))
+cm = m/100 
+print(f"you have {m}metr and convert to {cm}cm")
+
+
+
---
+++ zarb.py
@@ -0,0 +1,10 @@
+
+number1=int(input("plz enter number= "))
+number2=int(input("plz enter number= "))
+zarb=number1 * number2
+print(zarb)
+
+number_3=int(input("please enter number= "))
+number_4=int(input("please enter number="))
+add=number_3 + number_4
+print(add)
